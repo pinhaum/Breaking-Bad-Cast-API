bfa2990567b2c820a590a8bdabed5d9778ffe003
Gabriel Ramos <gcrepho@gmail.com>
2020-07-06 15:47:18 -0300

    added CharacterGrid and CharacterItem components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
 import Header from "./components/ui/header";
+import CharacterGrid from "./components/characters/CharacterGrid";
 
 const App = () => {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios(
+        `https://www.breakingbadapi.com/api/characters?`
+      );
+
+      console.log(result.data);
+
+      setItems(result.data);
+      setIsLoading(false);
+    };
+
+    fetchItems();
+  }, []);
+
   return (
     <div className="container">
       <Header />
+      <CharacterGrid isLoading={isLoading} items={items} />
     </div>
   );
 };
