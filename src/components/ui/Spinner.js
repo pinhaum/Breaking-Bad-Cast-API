12572d0b856f87e64fd3b874ed23e3f3954c595a
Gabriel Ramos <gcrepho@gmail.com>
2020-07-06 16:02:12 -0300

    added Spinner component for loading

---
+++ src/components/ui/Spinner.js
@@ -0,0 +1,8 @@
+import React from "react";
+import spinner from "../../img/spinner.gif";
+
+const Spinner = () => {
+  return <img src={spinner} alt="Loading" className="spinner" />;
+};
+
+export default Spinner;
